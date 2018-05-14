41fafd060d2d5e20e585c7e4c343ba7b5f89ea3d
Cassio Conti <cassiorconti@gmail.com>
2018-05-13 19:16:12 -0500

    Solution for part 2 of responsive images project

--- part2/responsive-images-project/Gruntfile.js
+++ part2/responsive-images-project/Gruntfile.js
@@ -44,24 +44,12 @@ module.exports = function (grunt) {
       },
     },
 
-    /* Copy the "fixed" images that don't go through processing into the images/directory */
-    copy: {
-      dev: {
-        files: [{
-          expand: true,
-          src: ['fixed/*.{gif,jpg,png}'],
-          cwd: 'src/app/images_src/',
-          dest: 'src/app/images/',
-          flatten: true,
-        }]
-      },
-    },
   });
 
   grunt.loadNpmTasks('grunt-responsive-images');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-mkdir');
-  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+  grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images']);
 
 };
