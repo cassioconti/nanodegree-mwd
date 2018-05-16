4dcb4769c92ba939297720e584f9dc59d3c29e6a
Cassio Conti <cassiorconti@gmail.com>
2018-05-15 22:12:09 -0500

    Generated new images and added them to source control. Remove the old images.

--- part2/responsive-images-project/Gruntfile.js
+++ part2/responsive-images-project/Gruntfile.js
@@ -1,11 +1,3 @@
-/*
- After you have changed any settings for the responsive_images task,
- run this with one of these options:
-  "grunt" alone creates a new, completed images directory
-  "grunt clean" removes the images directory
-  "grunt responsive_images" re-processes images without removing the old ones
-*/
-
 module.exports = function (grunt) {
 
   grunt.initConfig({
@@ -17,6 +9,10 @@ module.exports = function (grunt) {
             width: 1600,
             suffix: '_large_2x',
             quality: 30
+          }, {
+            width: 800,
+            suffix: '_large_1x',
+            quality: 50
           }]
         },
         files: [{
@@ -27,29 +23,9 @@ module.exports = function (grunt) {
         }]
       }
     },
-
-    /* Clear out the images directory if it exists */
-    clean: {
-      dev: {
-        src: ['src/app/images'],
-      },
-    },
-
-    /* Generate the images directory if it is missing */
-    mkdir: {
-      dev: {
-        options: {
-          create: ['src/app/images']
-        },
-      },
-    },
-
   });
 
   grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-mkdir');
-  grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images']);
+  grunt.registerTask('default', ['responsive_images']);
 
 };
