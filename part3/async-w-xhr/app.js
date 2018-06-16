2921f85ea75c8d7edfa3e4e500c301dbfdd84063
Cassio Conti <cassiorconti@gmail.com>
2018-06-15 21:31:43 -0500

    async xhr lesson project start files

---
+++ part3/async-w-xhr/app.js
@@ -0,0 +1,23 @@
+(function () {
+    const form = document.querySelector('#search-form');
+    const searchField = document.querySelector('#search-keyword');
+    let searchedForText;
+    const responseContainer = document.querySelector('#response-container');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        responseContainer.innerHTML = '';
+        searchedForText = searchField.value;
+
+        const unsplashRequest = new XMLHttpRequest();
+
+        unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        unsplashRequest.onload = addImage;
+
+        unsplashRequest.send()
+    });
+
+    function addImage(result) {
+        console.log(result);
+    }
+})();
