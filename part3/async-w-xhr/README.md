755f004576c149b73bde9ebd7f50abe56d590244
Cassio Conti <cassiorconti@gmail.com>
2018-06-16 16:21:24 -0500

    async with xhr solution

---
+++ part3/async-w-xhr/README.md
@@ -0,0 +1,15 @@
+# Async
+
+Create Your Accounts
+
+To complete these final steps, you'll need accounts with Unsplash and The New York Times.
+Unsplash
+
+    Create a developer account here - https://unsplash.com/developers
+    Next, create an application here - https://unsplash.com/oauth/applications
+        this will give you an "Application ID" that you'll need to make requests
+
+The New York Times
+
+    Create a developer account here - https://developer.nytimes.com/
+    They'll email you your api-key (you'll need this to make requests)
--- part3/async-w-xhr/app.js
+++ part3/async-w-xhr/app.js
@@ -10,14 +10,36 @@
         searchedForText = searchField.value;
 
         const unsplashRequest = new XMLHttpRequest();
-
         unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
         unsplashRequest.onload = addImage;
+        unsplashRequest.setRequestHeader('Authorization', 'Client-ID #####');
+        unsplashRequest.send();
 
-        unsplashRequest.send()
+        const articleRequest = new XMLHttpRequest();
+        articleRequest.onload = addArticles;
+        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=#####`);
+        articleRequest.send();
     });
 
     function addImage(result) {
-        console.log(result);
+        const data = JSON.parse(this.responseText);
+
+        if (data && data.results && data.results[0]) {
+            const firstImage = data.results[0];
+            htmlContent = `<figure>
+                <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+            </figure>`;
+
+            responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+        }
+    }
+
+    function addArticles() {
+        const data = JSON.parse(this.responseText);
+        data.response.docs.forEach(doc => {
+            htmlContent = `<p>${doc.snippet}</p>`;
+            responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+        });
     }
 })();
