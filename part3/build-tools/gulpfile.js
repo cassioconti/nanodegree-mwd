4902221cbfcb7944a5c437033e37049c68674889
Cassio Conti <cassiorconti@gmail.com>
2018-06-19 14:42:31 -0500

    Working with gulp. Watch for style changes

---
+++ part3/build-tools/gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('default', () => {
+    gulp.watch('sass/**/*.scss', gulp.series('styles'));
+});
+
+gulp.task('styles', (done) => {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('./css'));
+    done();
+});
