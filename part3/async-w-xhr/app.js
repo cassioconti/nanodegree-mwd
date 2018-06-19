5443df93f1925773d72017047a1665dc30fd2c1c
Cassio Conti <cassiorconti@gmail.com>
2018-06-19 11:24:36 -0500

    Replace jquery by fetch. Also, add error handlers

--- part3/async-w-xhr/app.js
+++ part3/async-w-xhr/app.js
@@ -9,28 +9,33 @@
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
 
-        $.ajax({
-            url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
-            headers: {
-                'Authorization': 'Client-ID ####'
-            }
-        }).done(addImage);
+        fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`, {
+                headers: {
+                    Authorization: 'Client-ID ####'
+                }
+            }).then(result => result.json())
+            .then(addImage)
+            .catch(e => requestError(e, 'image'));
 
-        $.ajax({
-            url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=####`
-        }).done(addArticles);
+        fetch(`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=####`)
+            .then(result => result.json())
+            .then(addArticles)
+            .catch(e => requestError(e, 'articles'));
     });
 
     function addImage(result) {
+        htmlContent = '';
         if (result && result.results && result.results[0]) {
             const firstImage = result.results[0];
             htmlContent = `<figure>
-                <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+                <img src="${firstImage.urls.small}" alt="${searchedForText}">
                 <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
             </figure>`;
-
-            responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+        } else {
+            htmlContent = '<p>Unfortunately, no image was returned for your search.</p>'
         }
+
+        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
     }
 
     function addArticles(result) {
@@ -38,5 +43,10 @@
             htmlContent = `<p>${doc.snippet}</p>`;
             responseContainer.insertAdjacentHTML('beforeend', htmlContent);
         });
+    }
+
+    function requestError(e, part) {
+        console.log(e);
+        responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning">Oh no! There was an error making a request for the ${part}.</p>`);
     }
 })();
