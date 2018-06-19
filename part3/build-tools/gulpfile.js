d984c924161900eab59ee7a603b8e9c7443680f2
Cassio Conti <cassiorconti@gmail.com>
2018-06-19 16:18:16 -0500

    Add eslint, browser auto refresh on watch

--- part3/build-tools/gulpfile.js
+++ part3/build-tools/gulpfile.js
@@ -1,17 +1,33 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-
-gulp.task('default', () => {
-    gulp.watch('sass/**/*.scss', gulp.series('styles'));
-});
+/*eslint-env node */
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const eslint = require('gulp-eslint');
 
 gulp.task('styles', (done) => {
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
-            browsers: ['last 2 versions']
+            browsers: ['last 2 versions'],
         }))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
     done();
+});
+
+gulp.task('lint', () => {
+    return gulp.src(['**/*.js', '!node_modules/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError())
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', gulp.series('styles', 'lint'), () => {
+    gulp.watch('sass/**/*.scss', gulp.series('styles'));
+    gulp.watch('**/*.js', gulp.series('lint'));
+    browserSync.init({
+        server: './',
+    });
 });
